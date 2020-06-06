4e6d8ef3eb80c3f479ddce03cea07d2b378e9fee
martin-banks <anotherbanksy@gmail.com>
2020-06-06 23:18:41 +1000

    adds layout ith canvas element for project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useRef,  } from "react"
 // import { Link } from "gatsby"
 import Styled from 'styled-components'
 
@@ -14,19 +14,98 @@ const Page = Styled.div`
 `
 
 
-const IndexPage = () => (
-  <Page>
-    <Layout>
-      <SEO title="Home" />
 
-      <Header />
+const IndexPage = () => {
+  // const refCanvas = useRef('refCanvas')
+  const canvasContainer = useRef()
 
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
+  const width = 1080
+  const height = 565
 
-    </Layout>
+  const canvas = document.createElement('canvas')
+  canvas.setAttribute('width', width)
+  canvas.setAttribute('height', height)
+  const ctx = canvas.getContext('2d')
+  canvas.style.border = 'solid 1px lime'
 
-  </Page>
-)
+  const image = document.createElement('img')
+  image.style.width = `${width}px`
+  image.style.height = `${height}px`
+  image.setAttribute('src', '/images/kilimanjaro.jpg')
+
+  setTimeout(() => {
+    ctx.drawImage(image, 0, 0)
+    canvasContainer.current.appendChild(canvas)
+    // canvasContainer.current.appendChild(image)
+    const imgData = ctx.getImageData(0,0, width, height)
+
+    const colors = []
+    console.log('imgData', imgData)
+    for (let i = 0; i < (imgData.data.length / 4); i += 1) {
+      colors.push([
+        imgData.data[(i * 4) + 0],
+        imgData.data[(i * 4) + 1],
+        imgData.data[(i * 4) + 2],
+        imgData.data[(i * 4) + 3],
+      ])
+    }
+
+    const sortedCanvas = document.createElement('canvas')
+    sortedCanvas.setAttribute('width', width)
+    sortedCanvas.setAttribute('height', height)
+    const sorted_ctx = sortedCanvas.getContext('2d')
+    sortedCanvas.style.border = 'solid 1px tomato'
+
+    const newData = ctx.createImageData(width, height)
+
+    console.log({ newData, colors })
+    const rects = colors
+      .sort((a, b) => {
+        if (a[0] > b[0]) return 1
+        if (a[0] < b[0]) return -1
+        return 0
+      })
+
+    console.log({ rects, newData })
+    console.log(imgData.data.length, newData.data.length)
+    setTimeout(() => {
+      for (let i = 0; i < rects.length; i++) {
+        newData.data[(i * 4) + 0] = rects[i][0]
+        newData.data[(i * 4) + 1] = rects[i][1]
+        newData.data[(i * 4) + 2] = rects[i][2]
+        newData.data[(i * 4) + 3] = rects[i][3]
+      }
+      setTimeout(() => {
+        canvasContainer.current.appendChild(sortedCanvas)
+        sorted_ctx.putImageData(newData, 0, 0)
+      }, 5000)
+    }, 5000)
+
+    console.log(imgData)
+  }, 1000)
+
+
+  console.log({ ctx })
+
+
+  return (
+    <Page>
+      <Layout>
+        <SEO title="Home" />
+
+        <Header />
+
+        <h1>Pixel sorter</h1>
+        {/* <img src="/images/kilimanjaro.jpg" /> */}
+
+        {/* <canvas ref={ refCanvas } /> */}
+        <div ref={ canvasContainer }></div>
+
+
+      </Layout>
+
+    </Page>
+  )
+}
 
 export default IndexPage
